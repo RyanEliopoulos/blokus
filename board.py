adf8d6859eef652da3c5d72fb9cda0be0325f187
Ryan <ryan.eliopoulos@gmail.com>
2020-01-27 22:02:51 -0800

    shape follows mouse now

--- board.py
+++ board.py
@@ -1,11 +1,59 @@
 from tkinter import *
 from tkinter import ttk
 
-ACTIVE_SQUARE = -1
+ACTIVE_SQUARE = -1      ## This tracks the blue fill magic I did
 COLUMNS     = 10
 BOX_WIDTH   = 50
 ROWS        = 10
 BOX_HEIGHT  = 50
+
+ACTIVE_SHAPE = None    ## Currently selected shape
+ACTIVE_X = -1           ## Calculate relative mouse movement. Then translate that to the squares
+ACTIVE_Y = -1
+
+
+
+
+"""
+    Called on <M1>.  Pick up shape and glue shape to cursor
+    
+"""
+
+def clickCheck(event):
+    global ACTIVE_SHAPE
+    global ACTIVE_X
+    global ACTIVE_Y
+
+    """
+        On M1 do we drop a currently-selected shape? 
+        Certainly if we just clicked on a different chape.
+    """
+
+    ACTIVE_X = event.x
+    ACTIVE_Y = event.y
+    print("checking mouse click")
+    for shape in shapes:
+        ## Iterate through shapes to detect if they were clicked on
+        if shape.selectionCheck(event.x, event.y):
+            ##
+            ## ACTIVE_SHAP
+            ## shape.update()
+            ##
+            ACTIVE_SHAPE = shape
+            print("OMG WE CLICKED ON A SHAPE!!")
+            break
+
+
+def shapeMovement(event):
+    global ACTIVE_SHAPE
+    global ACTIVE_X
+    global ACTIVE_Y
+    if ACTIVE_SHAPE != None:
+        print("Mouse_X:{}, Mouse_y: {}, ACTIVE_X: {}, ACTIVE_Y: {}".format(event.x, event.y, ACTIVE_X, ACTIVE_Y))
+        ACTIVE_SHAPE.update(event.x, event.y)
+        ACTIVE_X = event.x
+        ACTIVE_Y = event.y
+
 
 
 class Shape(object):
@@ -39,7 +87,40 @@ class Shape(object):
                 canvas.itemconfigure(new_square, fill='blue')
                 self.squares.append(new_square)
 
+    def update(self, mouse_x, mouse_y):
 
+        ## Load location variables
+        next_x = next_y = None          ## position to update to
+        x_diff = ACTIVE_X - mouse_x                          ## current shape position minus current mouse coords
+        y_diff = ACTIVE_Y - mouse_y
+
+        print('x_diff: {}, y_diff: {}'.format(x_diff, y_diff))
+        for squares in self.squares:
+            current_x, current_y, trash1, trash2 = self.canvas.coords(squares)
+            ### Determining direction and updating accordingly
+            # x first
+            if x_diff > 0:  # mouse is moving left
+                next_x = current_x - x_diff
+            else:
+                next_x = current_x - x_diff
+            # y last
+            if y_diff > 0:
+                next_y = current_y - y_diff
+            else:
+                next_y = current_y - y_diff
+            self.canvas.coords(squares, next_x, next_y, next_x+BOX_WIDTH, next_y+BOX_HEIGHT)
+
+
+
+
+
+
+    def selectionCheck(self, clicked_x, clicked_y):
+        for square in self.squares:
+            x, y, x2, y2 = self.canvas.coords(square)
+            if clicked_x >= x and clicked_x <= x2 and clicked_y >= y and clicked_y >= y2:
+                return True
+            return False
 
 
 
@@ -67,6 +148,7 @@ class Shape(object):
 
 
 def updateSquares(event):
+    shapeMovement(event)
     global ACTIVE_SQUARE
     #print("motion of the ocean: %, %" % event.x, event.y)
     print("motion of the ocean: {}, {}".format(event.x, event.y))
@@ -110,7 +192,8 @@ root.title("BlockOS")
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
 
-root.bind('<Motion>', updateSquares)
+#root.bind('<Motion>', updateSquares)
+#root.bind('<Motion>', shapeMovement)
 
 # primary frame
 mainframe = ttk.Frame(root)
@@ -120,7 +203,7 @@ mainframe.grid(column=0, row=0, sticky=(N, S, E, W))
 canvas = Canvas(mainframe, height=1000, width=1000)
 canvas.grid(column=0, row=0, sticky=(N, S, E, W))
 
-
+canvas.bind('<Motion>', shapeMovement)
 
 for i in range(COLUMNS):#
     for j in range(ROWS):#
@@ -129,11 +212,12 @@ for i in range(COLUMNS):#
         print(f"The value of  'value' is: {value}")#
 
 
-
+root.bind('<Button-1>', clickCheck)
 #### Testing the Shape object
+shapes = []
 
 shape = Shape(700, 600, ['-', 'D', 'R'], canvas)
-
+shapes.append(shape)
 
 #----
 root.mainloop()
