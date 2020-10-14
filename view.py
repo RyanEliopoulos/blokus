9884a8c172a36b694da176584b5de1c3306ffaba
Ryan <ryan.eliopoulos@gmail.com>
2020-10-14 13:42:25 -0700

    +endgame method/logic
    There is now a button to finalize the game. Button press displays final
    score, or scores in the event of a tie, and prompts users to play an
    additional game or exit.

--- view.py
+++ view.py
@@ -1,42 +1,47 @@
 """
-    Should have 1) initScreen: Method to construct all screen elements not spawned at creation time.
-                            So, the grid and the shapes.
-                2) updateScreen: Method to update the squares displayed on screen
-
-
                 So obviously the model knows each squareID. It knows tkinters generation algorithms.
+
+                1) Need to update the board with player turn indicator
+                2) Need button that allows players to indicate the game is over and point tallying should begin.
+
+
 """
 from tkinter import *
 from tkinter import ttk
+from tkinter import messagebox
+import time
 
 
 class View(object):
 
     def __init__(self, height, width):
-        ## Build tk components
+        # Build tk components
         self.root       = self._initRoot()
         self.mainframe  = self._initFrame()
         self.canvas     = self._initCanvas(height, width)
+        # callback functions
+        self.endgame_listener = None
+        self.reset_function = None
 
-    ## Used at creation
+    # Used at creation
     def _initRoot(self):
         new_root = Tk()
         new_root.title("Blockus")
         new_root.columnconfigure(0, weight=1)
         new_root.rowconfigure(0, weight=1)
         return new_root
-    ## Used at creation
+    # Used at creation
     def _initFrame(self):
         new_frame = ttk.Frame(self.root)
         new_frame.grid(column=0, row=0, sticky=(N, S, W, E))
         return new_frame
-    ## Used at creation
+    # Used at creation
     def _initCanvas(self, height, width):
         new_canvas = Canvas(self.mainframe, height=height, width=width)
         new_canvas.grid(column=0, row=0, sticky=(N, S, E, W))
         return new_canvas
 
-    ## Binding controller method to mouse movement
+    # Binding controller method to mouse movement
     def setMovementListener(self, listener):
         self.root.bind('<Motion>', listener)
 
@@ -44,29 +49,58 @@ class View(object):
     def setClickListener(self, listener):
         self.root.bind('<Button-1>', listener)
 
-    ## listen for
+    # listen for
     def setKeyListener(self, listener):
         self.root.bind('<Right>', listener)
         self.root.bind('<Up>', listener)
 
-    ## Initial screen state.
+    def set_endgame_listener(self, listener):
+        self.endgame_listener = listener
+
+    def set_reset(self, controller_reset_function):
+        self.reset_function = controller_reset_function
+
+    def reset(self, reset_bool):
+        self.reset_function(reset_bool)
+
+    def endgame(self):
+        print("In view endgame")
+        end_game_message = self.endgame_listener()
+        print(end_game_message)
+        #  Need to update the screen now instead of printing to console
+        #end_window = Toplevel(master=self.root)
+        #end_window.geometry("1500x1500")
+        #end_window.title(f"{results[0]} won the game!")
+        end_game_message += "\n\nWould you like to play again?"
+
+        if messagebox.askyesno("Game Over", end_game_message):
+            # reset board somehow here.
+            print("got the first string (I guess)")
+            self.reset(True)
+        else:
+            self.reset(False)
+
+    # Initial screen state.
     def initScreen(self, squares):
         """
         :param squares: list of coordinates and options used in canvas.create_rectangle.
                         Perhaps x,y,a,b, (fill string)
         :return: None
         """
-        print("heyo we here?")
         for opts in squares:
             new_item = self.canvas.create_rectangle(opts[0], opts[1], opts[2], opts[3], fill=opts[4])
             print(new_item)
 
-    ## Update the square positions on screen
+        # Constructing game end button
+        button = Button(self.mainframe, text="End Game", command=self.endgame)
+        button.grid(column=1, row=0, stick=(N, S, E, W))
+
+    # Update the square positions on screen
     def updateScreen(self, squares):
         if squares is None:
             return
         for square in squares:
-            ## This keeps growing longer...I guess this is why dicts are better??
+            # This keeps growing longer...I guess this is why dicts are better??
             x = square[0]
             y = square[1]
             x2 = square[2]
@@ -81,6 +115,6 @@ class View(object):
             if bring_fore:
                 self.canvas.tag_raise(item_number)
 
-    ## Begin main program loop
+    # Begin main program loop
     def beginLoop(self):
         self.root.mainloop()
