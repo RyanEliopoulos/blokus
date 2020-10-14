a805443eaad862bd6fdf816008934eca050e58cd
Ryan <ryan.eliopoulos@gmail.com>
2020-10-14 15:42:40 -0700

    +endgame, +turn indicator logic
    Should have committed after adding support for game end button. Oops.
    Now behaves more like an actual controller and updates the view with the
    current player.
    
    Endgame and reset_listener - in that order. Reset kills the GUI loop and
    pops back out within a loop OR exits game, depending on user input.

--- controller.py
+++ controller.py
@@ -2,18 +2,24 @@
 
 class Controller(object):
 
-
     def __init__(self, model, view):
         self.boardModel = model
         self.boardView = view
+
+        # Clunky way to tell main.py to instantiate a new instance of everything
+        # so we can play again
+        self.reset = False
 
         ##### init board
         ## Set listeners
         self.boardView.setMovementListener(self.mouseListener)
         self.boardView.setClickListener(self.clickListener)
         self.boardView.setKeyListener(self.keyListener)
+        self.boardView.set_endgame_listener(self.endgame_listener)
+        self.boardView.set_reset(self.reset_listener)
 
         self.boardView.initScreen(self.boardModel.initScreen())
+        self.boardView.update_turn_indicator(self.boardModel.current_player)
 
         ############################################
         ## Begin
@@ -27,7 +33,14 @@ class Controller(object):
     def clickListener(self, event):
         view = self.boardView
         model = self.boardModel
-        view.updateScreen(model.clickEvent(event))
+
+        # Updating "current player" indicator, if applicable
+        player_before_click = model.current_player
+        updated_coordinates = model.clickEvent(event)
+        if player_before_click != model.current_player:
+            view.update_turn_indicator(model.current_player)
+
+        view.updateScreen(updated_coordinates)
 
     def keyListener(self, event):
         view = self.boardView
@@ -36,3 +49,16 @@ class Controller(object):
         print("key listener activated")
         print(event.keysym)
 
+    def endgame_listener(self):
+        print('in controller endgame listener')
+        return self.boardModel.endgame()
+
+    def reset_listener(self, reset_bool):
+        self.reset = reset_bool
+        print(self.reset)
+        if self.reset:
+            print("Resetting")
+            self.boardView.root.destroy()
+        else:
+            print("exiting game")
+            exit(0)
